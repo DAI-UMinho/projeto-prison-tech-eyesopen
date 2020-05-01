cb58821ee397b278c2978a34482f300419e98f1f
Bitkas <61255210+Bitkas@users.noreply.github.com>
2020-05-01 19:24:06 +0100

    Ocorrencia
    submete mais que um recluso em cada ocorrencia

--- PDAI/PDAI/IncidentsAddForm.cs
+++ PDAI/PDAI/IncidentsAddForm.cs
@@ -25,16 +25,31 @@ namespace PDAI
         private void button1_Click(object sender, EventArgs e)
         {
             int row = dataGridView1.CurrentCell.RowIndex;
+            
+            
+            //MessageBox.Show("" + rowCount);
+            
             if (row >= dataGridView1.RowCount - 1)
             {
                 MessageBox.Show("Nao foi selecionado nenhum recluso!");
             }
             else
             {
-                string texto;
-                texto = "" + dataGridView1.Rows[row].Cells[0].Value + "-" + dataGridView1.Rows[row].Cells[1].Value;
-                pList.Text = texto;
-                this.Dispose();
+                string texto = "";
+                foreach (DataGridViewRow dgv in dataGridView1.SelectedRows)
+                {
+                    if (texto == "")
+                    {
+                        texto = "" + dgv.Cells[0].Value + "-" + dgv.Cells[1].Value;
+                    }
+                    else
+                    {
+                        texto += "-" + dgv.Cells[0].Value + "-" + dgv.Cells[1].Value;
+                    }
+                }
+                    pList.Text = texto;
+                    this.Dispose();
+                
             }
         }
     }
