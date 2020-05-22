8e45a6fdfb40ff66abda441bc71ad020ff0c1355
mjdsl13 <59835384+mjdsl13@users.noreply.github.com>
2020-05-22 17:17:13 +0100

    ASDF

---
+++ PDAI/PDAI/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace PDAI
+{
+    class Connection
+    {
+
+
+        //protected string connectionString = @"Data Source=DESKTOP-S8JGCRT;Initial Catalog=DAI_Database;User ID=sa;Password=dai";
+        //protected string connectionString = @"Data Source=LAPTOP-G0967CH5;Initial Catalog=DAI_Database;User ID=teste;Password=password";
+        //protected string connectionString = @"Data Source=OXYZ\PROJECTDEV;Initial Catalog=DAI_Database;User ID=sa;Password=ProjectDev";
+        //protected string connectionString = @"Data Source=LAPTOP-G0967CH5;Initial Catalog=DAI_Database;User ID=teste;Password=password";
+        protected string connectionString = @"Data Source=DESKTOP-SBQKKVO\DAI_SERVER;Initial Catalog=DAI_Database;User ID=sa;Password=dai";
+        //protected string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=DAI_Database;Integrated Security=True;";
+
+
+        protected SqlConnection sqlConn;
+        protected SqlCommand command;
+        protected SqlDataAdapter adapter;
+        protected SqlDataReader reader;
+        protected string sql;
+
+    }
+}
+
